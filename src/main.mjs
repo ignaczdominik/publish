2d5d50543da06907f0bffbf0494719a86906482c
Ignácz Dominik Bence <ignacz.dominik@njszg.hu>
2023-12-04 17:38:18 +0100

    F1

---
+++ src/main.mjs
@@ -0,0 +1,3 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import "@assets/css/style.css"
+import "bootstrap/dist/js/bootstrap"
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
+import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    
+    resolve:{
+        alias:{
+            "@": fileURLToPath(new URL('./src', import.meta.url)),
+            '@assets': fileURLToPath(new URL('./src/assets',import.meta.url))
+        }
+    },
+    server:{
+        port: 8080
+    }
 })

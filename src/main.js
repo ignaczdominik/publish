431d4e934715038bfb9fbaf6a45a34ad28da56d7
Ignácz Dominik Bence <ignacz.dominik@njszg.hu>
2023-12-04 17:20:49 +0100

    Add frontend resources

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import "@assets/css/style.css"
+import "bootstrap/dist/js/bootstrap"
+import forestImgUrl from "@assets/images/forest.jpg"
+
+document.querySelector(".card img").src = forestImgUrl;
